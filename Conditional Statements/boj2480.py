24eed3fecae86ed8e0f0347c2f4212b6652b0f94
sloth0423 <ymg579@naver.com>
2024-10-29 19:45:44 +0900

    2024-10-29

--- Conditional Statements/boj2480.py
+++ Conditional Statements/boj2480.py
@@ -11,5 +11,12 @@ elif( x == z and z != y):
     print(1000+x*100)
 elif( y == z and z != x):
     print(1000+y*100)
-elif( x != y and y != z):
-    print(max(x,y,z)*100)
+else:
+# elif( x != y and y != z and x != z): # 혼자 생쇼함 ㅋㅋ;
+    if(x>y and x>z):
+        print(x*100)
+    if(y>x and y>z):
+        print(y*100)
+    if(z>x and z>y):
+        print(z*100)
+    #print(max(x,y,z)*100) // max() 함수를 안다고 생각했을 때 풀이
