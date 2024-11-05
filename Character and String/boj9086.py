5938fb3e0677787f47a9c0b1f140f17e3ac44582
sloth0423 <ymg579@naver.com>
2024-11-05 19:44:59 +0900

    2024-11-05-(3)

--- Character and String/boj9086.py
+++ Character and String/boj9086.py
@@ -6,5 +6,7 @@ x = int(input())
 
 for _ in range(x):
     y = input()
-    print(y[0], end = "")
-    print(y[-1])
+    print(y[0],y[-1],sep = '')
+    # print(y[0], end = "")
+    # print(y[-1])
+
