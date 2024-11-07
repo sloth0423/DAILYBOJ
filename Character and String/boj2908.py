a6f19425c21c86b0de81a091e107a7a74ea206f1
sloth0423 <ymg579@naver.com>
2024-11-07 18:22:11 +0900

    2024-11-07-(3)

--- Character and String/boj2908.py
+++ Character and String/boj2908.py
@@ -7,8 +7,6 @@ x, y = input().split()
 x = list(x)
 y = list(y)
 
-print(x,y)
-
 x[0], x[2] = x[2], x[0]
 y[0], y[2] = y[2], y[0]
 
---
+++ Character and String/boj5622.py
@@ -0,0 +1,25 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+x = input()
+sum = 0
+for i in range(len(x)):
+    if x[i] == 'A' or x[i] == 'B' or x[i] == 'C':
+        sum += 3
+    if x[i] == 'D' or x[i] == 'E' or x[i] == 'F':
+        sum += 4
+    if x[i] == 'G' or x[i] == 'H' or x[i] == 'I':
+        sum += 5
+    if x[i] == 'J' or x[i] == 'K' or x[i] == 'L':
+        sum += 6
+    if x[i] == 'M' or x[i] == 'N' or x[i] == 'O':
+        sum += 7
+    if x[i] == 'P' or x[i] == 'Q' or x[i] == 'R' or x[i] == 'S':
+        sum += 8
+    if x[i] == 'T' or x[i] == 'U' or x[i] == 'V':
+        sum += 9
+    if x[i] == 'W' or x[i] == 'X' or x[i] == 'Y' or x[i] == 'Z':
+        sum += 10
+print(sum)
+
