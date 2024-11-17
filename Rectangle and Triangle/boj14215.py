70477731c4086f9ef799dd05d1a24770813bcc88
sloth0423 <ymg579@naver.com>
2024-11-17 22:33:33 +0900

    2024-11-17-(5)

--- Rectangle and Triangle/boj14215.py
+++ Rectangle and Triangle/boj14215.py
@@ -3,3 +3,12 @@ if max(ls) >= sum(ls) - max(ls):
     print((sum(ls)-max(ls))*2-1)
 else:
     print(sum(ls))
+
+# 바꾸기 전 코드
+# ls = list(map(int, input().split()))
+# a = max(ls)
+# b = sum(ls)
+# if a >= b - a:
+#     print((b - a) * 2 - 1)
+# else:
+#     print(b)
